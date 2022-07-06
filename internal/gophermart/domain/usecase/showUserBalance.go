36904e9efb3fdf17755b9417d848f5e87d888aac
latypov <aidlatyp@gmail.com>
2022-07-06 16:14:53 +0200

    fix lint

--- internal/gophermart/domain/usecase/showUserBalance.go
+++ internal/gophermart/domain/usecase/showUserBalance.go
@@ -2,7 +2,7 @@ package usecase
 
 import (
 	"context"
-  
+
 	"github.com/alexdyukov/gophermart/internal/gophermart/domain/core"
 	"github.com/alexdyukov/gophermart/internal/sharedkernel"
 )
@@ -31,7 +31,6 @@ func NewShowUserBalance(repo ShowUserBalanceRepository) *ShowUserBalance {
 		Repo: repo,
 	}
 }
-
 
 func (s *ShowUserBalance) Execute(ctx context.Context, user *sharedkernel.User) (*ShowUserBalanceOutputDTO, error) {
 	userAccount, err := s.Repo.FindAccountByID(ctx, user.ID())
