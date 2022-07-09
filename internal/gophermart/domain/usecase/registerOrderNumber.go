76a1e90881dea934bd014fba4b36a755590f2bb6
OlesyaBelochka <OlesyaBelle@gmail.com>
2022-07-09 18:54:08 +0300

    добавили комментарии

--- internal/gophermart/domain/usecase/registerOrderNumber.go
+++ internal/gophermart/domain/usecase/registerOrderNumber.go
@@ -50,21 +50,27 @@ func NewLoadOrderNumber(repo RegisterUserOrderRepository, gw CalculationStateGat
 func (ruo *RegisterUserOrder) Execute(ctx context.Context, number int, user *sharedkernel.User) error {
 
 	if !sharedkernel.ValidLuhn(strconv.Itoa(number)) {
-		fmt.Println("номер не прошел проверку луна ", number)
+		fmt.Println("#RegisterUserOrderPostHandler: номер не прошел проверку луна ", number)
 		return sharedkernel.ErrIncorrectOrderNumber
 	}
 
+	// вот тут выдает ошибку, не может что-то там обновить и дальше не идет.
 	inputDTO, err := ruo.ServiceGateway.GetOrderCalculationState(number)
+	fmt.Println("#RegisterUserOrderPostHandler: из аккурала получили такую структуру: ", inputDTO)
 	if err != nil {
-		return err // nolint:wrapcheck // ok
+
+		// return err // nolint:wrapcheck // ok
 	}
 
 	userOrder := core.NewOrderNumber(number, sharedkernel.Money(inputDTO.Accrual), user.ID(), inputDTO.Status)
 
+	fmt.Println("#RegisterUserOrderPostHandler: номер верный идем дальше, пробуем записать номер в БД ", number)
 	err = ruo.Repository.SaveUserOrder(ctx, &userOrder)
 	if err != nil {
+		fmt.Println("#RegisterUserOrderPostHandler:", err)
 		return err // nolint:wrapcheck // ok
 	}
 
+	fmt.Println("#RegisterUserOrderPostHandler: заказ в БД записан УСПЕШНО ", number)
 	return nil
 }
